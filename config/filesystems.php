50ea159e0f46a4bd985bcaad2f751aa91596cde2
ggavrushaa <gavrilnikitin2020@gmail.com>
2024-09-09 14:17:38 +0300

    fix bugs

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,13 +40,13 @@ return [
             'driver' => 'local',
             'root' => storage_path('app/public'),
             'url' => env('APP_URL').'/storage',
-        'visibility' => 'public',
+            'visibility' => 'public',
         ],
 
         'warranty_claims_files' => [
             'driver' => 'local',
             'root' => storage_path('app/warranty_claims_files'),
-            'url' => env('APP_URL').'/storage/warranty_claims_files',
+            'url' => env('APP_URL').'/storage/app/warranty_claims_files',
             'visibility' => 'public',
         ],
 
